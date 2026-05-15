bc226356c72f6f3b48d3de8e3f29c21f03242a14
Komi Adokpe <komiadokpe05@gmail.com>
2026-05-15 19:34:35 +0200

    Update login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,39 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+from core.logger import logger
+
+class LoginPage(BasePage):
+    # Locators pour les éléments de la page de connexion
+    USERNAME_INPUT = (By.ID, "user-name")
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_MESSAGE = (By.CSS_SELECTOR, "[data-test='error']")
+    
+    def open_login_page(self):
+        """Ouvre la page de connexion."""
+        logger.info("🌐 Ouverture de la page de connexion")
+        self.open()
+    
+    def login(self, username, password):
+        """Effectue une tentative de connexion avec les informations d'identification fournies."""
+        logger.info("📝 Saisie des identifiants de connexion")
+        self.input_text(self.USERNAME_INPUT, username)
+        self.input_text(self.PASSWORD_INPUT, password)
+        logger.info("🔐 Tentative de connexion")
+        self.click(self.LOGIN_BUTTON)
+        
+    def is_login_successful(self):
+        """Vérifie si la connexion a réussi en vérifiant que l'URL contient '/inventory'."""
+        logger.info("🔍 Vérification de la réussite de la connexion")
+        return self.wait_for_url_contains("/inventory")
+
+    def is_error_message_displayed(self):
+        """Vérifie si le message d'erreur est affiché après une tentative de connexion échouée."""
+        logger.info("🔍 Vérification de l'affichage du message d'erreur")
+        return self.is_element_visible(self.ERROR_MESSAGE)
+    
+    def get_error_message(self):
+        """Retourne le message d'erreur affiché après un login invalide."""
+        logger.info("📝 Récupération du message d'erreur")
+        return self.get_text(self.ERROR_MESSAGE)
