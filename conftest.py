12b24db57dff7b09f51afa1e3fe2df24b0861bb5
Komi Adokpe <komiadokpe05@gmail.com>
2026-05-17 15:05:24 +0200

    Update conftest.py - Script refactoring

--- conftest.py
+++ conftest.py
@@ -1,42 +1,83 @@
+# ==============================
+# ========== LIBRARY ===========
+# ==============================
+
 import pytest
+from core.utils import get_credentials, take_screenshot
 from core.driver_factory import create_driver
-from core.utils import take_screenshot
-from core.logger import logger
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+from pages.cart_page import CartPage
+
+
+# ==============================
+# ========== FIXTURES ==========
+# ==============================
 
 @pytest.fixture
 def driver():
-    """Fixture principale Selenium WebDriver."""
-    logger.info("🧭 Initialisation du navigateur")
-
+    """Initialise et ferme le WebDriver Selenium."""
     driver = create_driver()
-
     yield driver
-
-    logger.info("🛑 Fermeture du navigateur\n")
     driver.quit()
-            
+
+
+@pytest.fixture
+def login_page(driver):
+    """Retourne une instance de la page Login."""
+    return LoginPage(driver)
+
+
+@pytest.fixture
+def inventory_page(driver):
+    """Retourne une instance de la page Inventory."""
+    return InventoryPage(driver)
+
+
+@pytest.fixture
+def cart_page(driver):
+    """Retourne une instance de la page Cart."""
+    return CartPage(driver)
+
+
+@pytest.fixture
+def logged_user(driver, login_page, inventory_page):
+    """Connecte un utilisateur valide et retourne la page Inventory."""
+    creds = get_credentials("valid_user")
+
+    login_page.open_page()
+    login_page.login(creds["username"], creds["password"])
+    login_page.wait_inventory_page()
+
+    return inventory_page
+
+
+# ==============================
+# ============ HOOKS ===========
+# ==============================
+
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     """
     Hook exécuté après chaque phase de test.
-    Capture screenshot uniquement en cas d'échec.
+    Capture automatiquement une screenshot en cas d'échec.
     """
-
     outcome = yield
     result = outcome.get_result()
 
-    # On ne traite que la phase d'exécution du test
     if result.when != "call":
         return
 
     if result.failed:
-        logger.error(f"❌ TEST ECHOUE : {item.name}")
-
         driver = item.funcargs.get("driver")
 
         if driver:
             try:
                 path = take_screenshot(driver, f"FAILED_{item.name}")
-                logger.error(f"📸 Screenshot sauvegardée: {path}")
             except Exception as e:
-                logger.error(f"⚠️ Screenshot échouée: {e}")
+                pass
+
+
+
+
+
