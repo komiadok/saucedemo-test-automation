a882c9b44dc048e5ae9cae82b7fc577eff8236fc
Komi Adokpe <komiadokpe05@gmail.com>
2026-05-17 19:20:59 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
-# ==============================
-# ========== LIBRARY ===========
-# ==============================
+# ===========================================
+# ================= LIBRARY =================
+# ===========================================
 
 import pytest
 from core.utils import get_credentials, take_screenshot
@@ -10,9 +10,9 @@ from pages.inventory_page import InventoryPage
 from pages.cart_page import CartPage
 
 
-# ==============================
-# ========== FIXTURES ==========
-# ==============================
+# ===========================================
+# ================ FIXTURES =================
+# ===========================================
 
 @pytest.fixture
 def driver():
@@ -21,40 +21,36 @@ def driver():
     yield driver
     driver.quit()
 
-
 @pytest.fixture
 def login_page(driver):
     """Retourne une instance de la page Login."""
     return LoginPage(driver)
-
 
 @pytest.fixture
 def inventory_page(driver):
     """Retourne une instance de la page Inventory."""
     return InventoryPage(driver)
 
-
 @pytest.fixture
 def cart_page(driver):
     """Retourne une instance de la page Cart."""
     return CartPage(driver)
-
 
 @pytest.fixture
 def logged_user(driver, login_page, inventory_page):
     """Connecte un utilisateur valide et retourne la page Inventory."""
     creds = get_credentials("valid_user")
 
-    login_page.open_page()
+    login_page.open_login_page()
     login_page.login(creds["username"], creds["password"])
     login_page.wait_inventory_page()
 
     return inventory_page
 
 
-# ==============================
-# ============ HOOKS ===========
-# ==============================
+# ===========================================
+# ================== HOOKS ==================
+# ===========================================
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
