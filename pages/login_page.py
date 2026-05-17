fd648ee51e2a0cd928e9e09bb67eff88ba1e090e
Komi Adokpe <komiadokpe05@gmail.com>
2026-05-17 14:55:38 +0200

    Update login_page.py - Script Refactoring

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,43 +1,52 @@
-from pages.base_page import BasePage
-from selenium.webdriver.common.by import By
+# ==============================
+# ========== LIBRARY ===========
+# ==============================
 
-from core.logger import logger
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+# ==============================
+# ============ PAGE ============
+# ==============================
 
 class LoginPage(BasePage):
-    # Locators pour les éléments de la page de connexion
+    
+    # ==============================
+    # ========== LOCATORS ==========
+    # ==============================
+    
     USERNAME_INPUT = (By.ID, "user-name")
     PASSWORD_INPUT = (By.ID, "password")
-    LOGIN_BUTTON = (By.ID, "login-button")
+    LOGIN_BTN = (By.ID, "login-button")
     ERROR_MESSAGE = (By.CSS_SELECTOR, "[data-test='error']")
     
+    # ==============================
+    # ========== ACTIONS ==========
+    # ==============================
+    
     def open_login_page(self):
-        """Ouvre la page de connexion."""
-        logger.info("🌐 Ouverture de la page de connexion")
+        """Ouvre la page de connexion avec le chemin fourni (optionnel)."""
         self.open()
     
     def login(self, username, password):
         """Effectue une tentative de connexion avec les informations d'identification fournies."""
-        logger.info("📝 Saisie des identifiants de connexion")
-        self.input_text(self.USERNAME_INPUT, username)
-        self.input_text(self.PASSWORD_INPUT, password)
-        logger.info("🔐 Tentative de connexion")
-        self.click(self.LOGIN_BUTTON)
-        
-    def is_login_successful(self):
-        """Vérifie si la connexion a réussi en vérifiant que l'URL contient '/inventory'."""
-        logger.info("🔍 Vérification de la réussite de la connexion")
-        return self.wait_for_url_contains("/inventory")
-
-    def is_error_message_displayed(self):
-        """Vérifie si le message d'erreur est affiché après une tentative de connexion échouée."""
-        logger.info("🔍 Vérification de l'affichage du message d'erreur")
-        return self.is_element_visible(self.ERROR_MESSAGE)
+        self.type(self.USERNAME_INPUT, username)
+        self.type(self.PASSWORD_INPUT, password)
+        self.click(self.LOGIN_BTN)
     
+    # ==============================
+    # ========== GETTERS ===========
+    # ==============================
+        
     def get_error_message(self):
         """Retourne le message d'erreur affiché après un login invalide."""
         return self.get_text(self.ERROR_MESSAGE)
     
-    def is_error_message_valid(self, expected_message):
-        """Vérifie que le message d'erreur affiché correspond au message attendu."""
-        return self.is_text_present(self.ERROR_MESSAGE, expected_message)
+    # ==============================
+    # ========== WAITERS ==========
+    # ==============================
+        
+    def wait_inventory_page(self):
+        """Attend que l'URL de la page contienne 'inventory'."""
+        return self.wait_for_url_contains("inventory")
     
